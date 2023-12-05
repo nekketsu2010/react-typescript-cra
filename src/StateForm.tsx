3b5926735ed94a3f081d3d058fef9e0305074cfc
nekketsu2010 <>
2023-12-06 08:21:06 +0900

    feature: #5 基本的なフォームの実装をした。

---
+++ src/StateForm.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+export default function StateForm() {
+    const [form, setForm] = useState({
+        name: '山田太郎',
+        age: 18
+    });
+
+    const handleForm = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setForm({
+            ...form,
+            [e.target.name]: e.currentTarget.value
+        });
+    };
+
+    const show = () => {
+        console.log(`こんにちは${form.name}(${form.age}歳)さん`);
+    };
+
+    return (
+        <form>
+            <div>
+                <label htmlFor="name">名前</label>
+                <input id="name" name="name" type="text" value={form.name} onChange={handleForm} />
+            </div>
+            <div>
+                <label>年齢</label>
+                <input id="age" name="age" type="number" value={form.age} onChange={handleForm} />
+            </div>
+            <button type="button" onClick={show}>送信</button>
+        </form>
+    );
+}
