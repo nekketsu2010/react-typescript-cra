9336a936b447fc50e814687495d053d5310120e7
nekketsu2010 <>
2024-01-07 16:08:47 +0900

    feature: #7 背景色の変更と、ビューポートに規定で用意されているものを設定した。

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,5 +1,5 @@
 import type { Preview } from "@storybook/react";
-
+import { INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
@@ -9,6 +9,16 @@ const preview: Preview = {
         date: /Date$/i,
       },
     },
+    backgrounds: {
+      values: [
+        { name: 'ghostwhite', value: '#f8f8ff' },
+        { name: 'aquamarine', value: '#7fffd4' },
+        { name: 'coral', value: '#ff7f50' },
+      ],
+    },
+    viewport: {
+      viewports: INITIAL_VIEWPORTS
+    }
   },
 };
 
