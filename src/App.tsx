61a46d0857cad4d4dfc8410c1e987fbd41787680
nekketsu2010 <>
2024-01-14 10:00:44 +0900

    feature: #8 useRefを使用し、値を保持するようにした。

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 // import './App.css';
-import HookRefNg from './HookRefNg';
+import HookRef from './HookRef';
 
 function App() {
   return (
     <div className="App">
-      <HookRefNg />
+      <HookRef />
     </div>
   );
 }
